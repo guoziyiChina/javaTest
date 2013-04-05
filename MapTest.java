fe9a9cfb323282ce60827fdde4093d44c3ee86db
guoziyi <2565525659@qq.com>
2013-04-05 03:00:04 -0700

    atuo-boxing

--- MapTest.java
+++ MapTest.java
@@ -6,8 +6,17 @@ class MapTest
   public static void main(String[] args){
   HashMap m1 = new HashMap();
   HashMap m2 = new HashMap();
+  HashMap m4 = new HashMap();
+  
+  m4.put(1,1000);
+  m4.put(2,10000);
+  m4.put(3,1000000);
+  p(m4);
+  p((Integer)m4.get(3));
+  
   
   m1.put("a",new Integer(1));
+  
   m1.put("b",new Integer(2));
   m1.put(1,new String("haha"));
   p(m1);
