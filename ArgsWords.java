54996820df201766aad4a790158b3e1753eeb985
guoziyi <2565525659@qq.com>
2013-04-05 03:58:46 -0700

    atuo-boxing-args-words

---
+++ ArgsWords.java
@@ -0,0 +1,38 @@
+package guohao;
+import java.util.*;
+class ArgsWords
+{
+  private static final Integer ONE = new Integer(1);
+  public static void main(String[] args)
+  {
+    Map m = new HashMap();
+	for(int i= 0;i<args.length;i++)
+	{
+	  Integer freq = (Integer)m.get(args[i]);
+	  m.put(args[i],(freq == null? ONE: new Integer(freq.intValue() + 1)));
+	}
+	
+	System.out.println(m.size() + " distinct words found:");
+	System.out.println(m);
+  }
+}
+
+
+
+class ArgsWordsAutoBox
+{
+  private static final int ONE = 1;
+  public static int freq = 0;
+  public static void main(String[] args)
+  {
+    Map m = new HashMap();
+	for(int i= 0;i<args.length;i++)
+	{
+	  int freq = (Integer)m.get(args[i]) == null ? 0 : (Integer)m.get(args[i]);  // notice that if null or not,or you will get NULLPOINT EXCEPTION
+	  m.put(args[i],(freq == 0? ONE: (freq+ 1)));
+	}
+	
+	System.out.println(m.size() + " distinct words found:");
+	System.out.println(m);
+  }
+}
